11aa94d3b7b98ebfeae4231b237b683ba5b480b7
Denis Shevchenko <webmaster@gartal.cz>
2023-09-26 16:05:30 +0400

    Change messages

--- messages.py
+++ messages.py
@@ -3,13 +3,13 @@ messages = {
     "generate_report_button" : "Запросить отчет",
     "welcome_message" : "Добро пожаловать в наш бот, здесь вы можете добавить заявки на вакансии или запросить отчет о работе.",
     "send_link_message" : "Пришлите ссылку на интересующую вас вакансию",
-    "select_action_message" : "Выбирите действие",
+    "select_action_message" : "Выберите действие",
     "you_not_our_client_message" : "Вы не являетесь нашим клиентом",
-    "vacancy_link_is_not_valid_message" : "Кажется ваша ссылка не валидна, попробуйте еще раз!",
+    "vacancy_link_is_not_valid_message" : "Кажется, ваша ссылка некорректна, попробуйте еще раз!",
     "vacancy_in_processing_message" : "Мы обработаем вашу задачу в скором времени!",
     "something_went_wrong_message" : "Что-то пошло не так!",
-    "report_is_not_ready_message" : "Отчет еще готов, попробуйте позже",
-    "report_is_ready_message" : "Вот ваш отчет:",
-    "report_link_button" : "Перейти в отчету",
-    "report_is_in_generating_message" : "Идет генерация отчета..."
+    "report_is_not_ready_message" : "Отчет еще не готов, попробуйте позже",
+    "report_is_ready_message" : "Ваш отчет готов:",
+    "report_link_button" : "Перейти к отчету",
+    "report_is_in_generating_message" : "Идет подготовка отчета..."
 }
